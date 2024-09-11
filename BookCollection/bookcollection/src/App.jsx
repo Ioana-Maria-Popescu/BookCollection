17a6a2a6a17e1d07251d685fdf9e0ddd7ee9b2e2
Ioana Popescu <mariap1313@yahoo.com>
2024-09-11 21:40:53 +0300

    - added triangles in a separate folrder (Layout) - added a welcome page

--- BookCollection/bookcollection/src/App.jsx
+++ BookCollection/bookcollection/src/App.jsx
@@ -1,17 +1,16 @@
-import React from 'react';
+//import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div id="root">
-      <div className="corner top-left"></div>
-      <div className="corner top-right"></div>
-      <div className="corner bottom-left"></div>
-      <div className="corner bottom-right"></div>
-      <h1>Book Collection</h1>
-      {/* Your app content goes here */}
-    </div>
-  );
+    return (
+        <div className="actionButtons">
+            <h1>Welcome to Book Collection</h1>
+            <div>
+                <button>Login</button>
+                <button>Sign up</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ BookCollection/bookcollection/src/Layout.jsx
@@ -0,0 +1,14 @@
+function Layout() {
+
+    return (
+        <div id="root">
+            <div className="corner top-left"></div>
+            <div className="corner top-right"></div>
+            <div className="corner bottom-left"></div>
+            <div className="corner bottom-right"></div>
+            {/* Your app content goes here */}
+        </div>
+    )
+}
+
+export default Layout;
